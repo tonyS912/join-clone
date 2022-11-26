2f326efd9b1c6f09a57b8007c17ddebbc7d599dc
Majda-source <halim.majda@gmail.com>
2022-11-26 15:00:58 +0100

    index.html, signup.html, login.css, login.js geapdated

---
+++ assests/js/login.js
@@ -0,0 +1,10 @@
+function loginAsGuest() {
+    // document.getElementById('email-login').value = ''; 
+    // document.getElementById('password-login').value = '';
+    window.location.href = 'summary.html';
+    console.log('summary')
+}
+
+function showSignup() {
+    window.location.href = 'signup.html';
+}
