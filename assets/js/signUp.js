2fa3bcf73cfaf8c23b2ecd2f5dfda23806835100
Tony <schillertony@hotmail.de>
2022-12-04 19:19:36 +0100

    Implement Majdas changes include Backend

---
+++ assets/js/signUp.js
@@ -0,0 +1,37 @@
+async function signUp() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let searchExistingEmail = users.find(u => u.email == email);
+    
+    if (searchExistingEmail) {
+        userAlreadyExists();
+    } else {
+        users.push({ 'name': name, 'email': email.toLowerCase(), 'password': password });
+        await pushUsersToServer();
+        setTimeout(() => {
+            showLoginAfterSignup();
+        }, 500);
+    }
+}
+
+async function pushUsersToServer() {
+    await backend.setItem('users', JSON.stringify(users)); 
+}
+
+
+function showLoginAfterSignup() {
+    window.location.href = 'index.html?msg=Du hast dich ergolreich registriert';
+}
+
+
+function userAlreadyExists() {
+    alert('already exist');
+}
+
+
+
+
+function backToLogin() {
+    window.location.href = 'index.html';
+}
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-setURL('http://developerakademie.com/smallest_backend_ever');
+setURL('https://gruppe-384.developerakademie.net/smallest_backend_ever');
+
+async function init() {
+        await downloadFromServer();
+        users = await JSON.parse(backend.getItem('users')) || [];
+        console.log('loaded');
+       
+}
