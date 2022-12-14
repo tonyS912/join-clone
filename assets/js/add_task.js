9333fa87011ebe8bfa8e23bbe3f1980431a137bb
Tony <schillertony@hotmail.de>
2022-12-14 20:26:49 +0100

    upgrade add task js-doc

--- assets/js/add_task.js
+++ assets/js/add_task.js
@@ -1,33 +1,62 @@
 let allTasks = [];
-let category = ['New Category', 'Sales', 'Backoffice'];
-let catColor = ['#0000FF', '#00FF00', '#FF0000', '#800080', '#F0F8FF', '#FF69B4'];
+let category = ["Sales", "Backoffice"];
+let catColor = [
+    "#0000FF",
+    "#00FF00",
+    "#FF0000",
+    "#800080",
+    "#F0F8FF",
+    "#FF69B4",
+];
 
 function addingTask() {
-  let title = document.getElementById('title').value;
-  let description = document.getElementById('description').value;
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
 
-  let task = {
-    'title': title,
-    'description': description
-  };
+    let task = {
+        title: title,
+        description: description,
+    };
 
-  allTasks.push(task);
+    allTasks.push(task);
 
-  let allTasksAsString = JSON.stringify(allTasks);
-  localStorage.setItem('allTasks', allTasksAsString);
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem("allTasks", allTasksAsString);
 }
 
 function loadTasks() {
-  let allTasksAsString = localStorage.getItem('allTasks');
-  allTasks = JSON.parse(allTasksAsString);
+    let allTasksAsString = localStorage.getItem("allTasks");
+    allTasks = JSON.parse(allTasksAsString);
 }
 
 function expandCategory() {
-  document.getElementById('category').classList.add("d-none");
-  document.getElementById('category-list').classList.remove("d-none");
+    document.getElementById("category").classList.add("d-none");
+    document.getElementById("category-list").classList.remove("d-none");
+
+    appendCategory();
+}
+
+function appendCategory() {
+    let cat = document.getElementById("category-list");
+    cat.innerHTML = "";
+
+    cat.innerHTML += selectCat();
+    cat.innerHTML += addOneCat();
+
+    for (let i = 0; i < category.length; i++) {
+        const elemnt = category[i];
+
+        cat.innerHTML += categoryParam(elemnt);
+    }
 }
 
 function miniCategory() {
-  document.getElementById('category').classList.remove('d-none');
-  document.getElementById('category-list').classList.add('d-none');
+    document.getElementById("category").classList.remove("d-none");
+    document.getElementById("category-list").classList.add("d-none");
+}
+
+function changeCategory() {
+  let addCat = document.getElementById('addOneCat');
+
+
 }
--- assets/js/add_task_templates.js
+++ assets/js/add_task_templates.js
@@ -1,7 +1,12 @@
-function Category() {
-  return /*html*/ '<li class="list-elemnt">Select task Category</li>';
+function categoryParam(elemnt) {
+    return /*html*/ `<li class="list-elemnt">${elemnt}</li>`;
 }
 
-function CategoryParam(element) {
-  return /*html*/ '<li class="list-elemnt">${element}</li>';
+function selectCat() {
+    return /*html*/ '<li onclick="miniCategory()" class="list-elemnt">Select task Category</li>';
 }
+
+function addOneCat() {
+    return /*html*/ '<li id="addOneCat" onclick="changeCategory()" class="list-elemnt">New Category</li>';
+}
+
