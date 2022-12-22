3ce8c161fd0a6861b8c2eb485f7175382e3c1ce0
Majda-source <halim.majda@gmail.com>
2022-12-22 15:46:46 +0100

    add toggled a. animated logOut on Board.html

--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,3 +1,5 @@
+
+let users = [];
 async function signUp() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
--- script.js
+++ script.js
@@ -5,3 +5,18 @@ async function init() {
         users = await JSON.parse(backend.getItem('users')) || [];
    
 }
+
+
+function showLogOut() {
+        let log_out = document.getElementById('log_out');
+        if (log_out.classList.contains('d-none')) {
+            log_out.style.animation = 'fade_in 0.5s ease-in-out';
+            log_out.classList.remove('d-none');
+        } else {
+            log_out.style.animation = 'fade_out 0.5s ease-out';
+            setTimeout(() => {
+                log_out.classList.add('d-none');
+            }, 400);
+        }
+        console.log('logout')
+    }
