2ba54462f50e373e662c35a3884d2842b76ff48e
Majda-source <halim.majda@gmail.com>
2022-12-30 17:26:24 +0100

    animated Logo password visibility added

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,75 @@
-    
-    
-    function loadMessage(){
-        const urlParams = new URLSearchParams(window.location.search);
-        const msg = urlParams.get('msg');
-        if(msg){
-            msgBox.innerHTML = msg;
+
+// Get a reference to the password field and image
+var passwordField = document.getElementById("password");
+var image = document.getElementById("passwordImg");
+
+// Add an event listener to the password field to listen for the input event
+passwordField.addEventListener("input", function() {
+  // If the password field has a value, change the image
+  if (passwordField.value) {
+    image.src = "../assets/img/hide.svg";
+  }
+  // If the password field is empty, change the image back to the default
+  else {
+    image.src = "../assets/img/lock.svg";
+  }
+});
+
+async function init() {
+    setURL('https://gruppe-384.developerakademie.net/smallest_backend_ever');
+        await downloadFromServer();
+        users = await JSON.parse(backend.getItem('users')) || [];
+}
+
+function animatedLogo(){
+        document.getElementById('logo').style = 'transform: translateX(-100%) translateY(-100%); left: 120px; top: 130px; scale: 1.0;';
+}
+
+
+function toggleVisibility(){
+              
+        // Get the current type of the password field (should be "password")
+        var currentType = passwordField.type;
+      
+        // If the current type is "password", change it to "text" to show the password
+        if (currentType == "password") {
+          passwordField.type = "text";
+          image.src = "../assets/img/eye.svg";
         }
+        // If the current type is "text", change it back to "password" to hide the password again
+        else {
+          passwordField.type = "password";
+          image.src = "../assets/img/hide.svg";
+        }
+      }
+
+
+function loadMessage(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const msg = urlParams.get('msg');
+    if(msg){
+        msgBox.innerHTML = msg;
     }
+}
+
+
+
+function showLogOut() {
+        let log_out = document.getElementById('log_out');
+        if (log_out.classList.contains('d-none')) {
+            log_out.style.animation = 'fade_in 0.5s ease-in-out';
+            log_out.classList.remove('d-none');
+        } else {
+            log_out.style.animation = 'fade_out 0.5s ease-out';
+            setTimeout(() => {
+                log_out.classList.add('d-none');
+            }, 400);
+        }
+        console.log('logout')
+    }
+    
+    
+   
 
 
    
